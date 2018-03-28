f3619e7038af09c3b2f2ba6572997a21b038a839
ityuhao <ityuhao@gmail.com>
2018-03-28 18:20:48 +0800

    cpw

--- assets/resources/script/do/allclose.js
+++ assets/resources/script/do/allclose.js
@@ -10,6 +10,9 @@ cc.Class({
         cc.weijifen.dialog.put(dialog);
     },
     closeMenu: function(){
+        var web = cc.find('Canvas').children[3].children[2].getComponent(cc.WebView);
+        web.url = '';
+
         let menu = cc.find("Canvas/menu");
         cc.weijifen.menu.put(menu);
     },
