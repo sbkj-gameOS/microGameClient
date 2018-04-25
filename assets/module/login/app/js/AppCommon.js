389d8d6f5c32c4d5a14c5380e2e84bd7984911bc
14768776836 <784700109@qq.com>
2018-04-25 17:20:24 +0800

    ts

--- assets/module/login/app/js/AppCommon.js
+++ assets/module/login/app/js/AppCommon.js
@@ -76,7 +76,7 @@ cc.Class({
         };
         
         //获取分享进入的时候，是否分享的游戏房间
-        var res = jsb.reflection.callStaticMethod("org/cocos2dx/javascript/event/EventManager", "raiseEvent", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;", "shareParam",result);
+        var res = jsb.reflection.callStaticMethod("org/cocos2dx/javascript/event/EventManager", "raiseEvent", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;", "shareParam","");
         self.alert("shareParam-res:"+res);
         if(res){
             //游戏房间
