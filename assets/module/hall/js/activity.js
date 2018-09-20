e6fd7b3026095048d4b7a1cd73714bd9a047b849
toby <2069420645@qq.com>
2018-09-20 19:08:14 +0800

    cpw

--- assets/module/hall/js/activity.js
+++ assets/module/hall/js/activity.js
@@ -3,15 +3,23 @@ cc.Class({
     extends: WJFCommon,
 
     properties: {
+        pointer: {
+            default: null,
+            type: cc.Sprite
+        },
         turnModel: cc.Node,
-        rightModel: cc.Node
+        turnImgArr: {
+            default: [],
+            type: cc.SpriteFrame
+        },
+        turnTable: cc.Node,
     },
     onLoad () {
         cc.weijifen.activityId = 31;
+        this.clickFlag = true; // 是否已经点击抽奖按钮
+        this.turnTableNum = 8; // 转盘格数 
+        this.turnRotation = 360 / (this.turnTableNum); // 每一份转盘格数转动的角度
         this.turnTableInit();
-
-
-
 
     },
     /**
@@ -33,16 +41,60 @@ cc.Class({
      "prizeUser":"OOO 获得 房卡-3张, T。篮球疯子爱健身 获得 房卡-1张  , Er. 获得 房卡-1张   ,T。篮球疯子爱健身 获得 房卡-1张,  亚茹[微笑] 获得 房卡-1张 ,  宋静冰 获得 房卡-1张 ","success":true}*/
         let self = this;
         function initData (res) {
-            var data1 = [];
+            var data1 = null;
             var data = JSON.parse(res);
             if (data.success) {
-
+                let awardData = JSON.parse(data.activityManange.awardData).awardData;
+                self.glData = JSON.parse(data.activityManange.awardData).glData;
+                // 转盘
+                for (let i = 0;i < awardData.length;i++) {
+                    let turnModel = cc.instantiate(self.turnModel);
+                    turnModel.getChildByName('txt').getComponent(cc.Label).string = awardData[i].nameValue;
+                    if (awardData[i].dname == '房卡') {
+                        turnModel.getChildByName('img').getComponent(cc.Sprite).spriteFrame = self.turnImgArr[0];
+                    } else if (awardData[i].dname == '再接再厉' || awardData[i].dname == '不要灰心') {
+                        turnModel.getChildByName('img').getComponent(cc.Sprite).spriteFrame = self.turnImgArr[1];
+                    } else if (awardData[i].dname == '月赛卡') {
+                        turnModel.getChildByName('img').getComponent(cc.Sprite).spriteFrame = self.turnImgArr[0];
+                    }
+                    turnModel.rotation = self.turnRotation * (i + 1);
+                    turnModel.parent = self.turnTable;
+                    turnModel.active = true;
+                }
+                // 右侧信息
 
 
             }
         };
         cc.weijifen.http.httpPost("/gamePrizeActivity/findActivityOne",{activityId:cc.weijifen.activityId},initData,self.err,self);
+    },
+    /*点击抽奖按钮*/
+    startClick: function () {
+        let self = this;
+        self.lottery();
 
+        self.pointerButtonStartControl();
+
+
+    },
+    /*查询当前用户是否有资格*/
+    lottery () {
+        let self = this;
+        function query (res) {
+            console.log(res)
+
+
+        }
+        cc.weijifen.http.httpPost('/gamePrizeActivity/findZiGe',{activityId:cc.weijifen.activityId},query,self.err,self);
+    },
+    /*转盘转动动画*/
+    pointerButtonStartControl: function () {
+        let self = this;
+        let angle = 200;// 初始状态下，抽奖结果所在的度数
+        let rounds = 6; // 转盘转动的圈数
+        let clickTimes = 6; // 转盘转动的时间
+        var rotateBy02 = cc.rotateBy(clickTimes, angle + 360 * rounds - self.turnRotation);
+        self.pointer.node.runAction(rotateBy02).easing(cc.easeCubicActionOut(clickTimes));
     },
     err () {
         console.log('错误！')
