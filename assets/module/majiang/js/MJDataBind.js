17c35a77ba3d3643af92adfd8f529238b6b22ce6
ityuhao@gmail.com <qwer1234>
2019-01-29 14:06:54 +0800

    mc
    注释掉android定位测试

--- assets/module/majiang/js/MJDataBind.js
+++ assets/module/majiang/js/MJDataBind.js
@@ -1137,23 +1137,23 @@ cc.Class({
         // 地理位置
         // 调用android方法名：getLocation
         // 返回地址位置：lo经度；alt，海拔；t时间
-        //  if (cc.sys.localStorage.getItem('tips') == 'false') {
-            var result = jsb.reflection.callStaticMethod("org/cocos2dx/javascript/event/EventManager", "raiseEvent", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;", 'getLocation','');
-            context.__proto__.__proto__.alert('位置：'+result);
-            if (result) {
-                let res = JSON.parse(result);
-                console.log('经纬度： ',res)
-                let params = {
-                    token:cc.weijifen.authorization,
-                    lng: res.lo,//j
-                    lat: res.la,//w
-                    alt:res.alt,
-                    t:res.t,
-                } 
-                //  cc.sys.localStorage.setItem('tips','true');
-                cc.weijifen.http.httpPost('/userInfo/position/save',params,this.getPosition,this.getErr,this) ;            
-            }
-        //  } 
+        // //  if (cc.sys.localStorage.getItem('tips') == 'false') {
+        //     var result = jsb.reflection.callStaticMethod("org/cocos2dx/javascript/event/EventManager", "raiseEvent", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;", 'getLocation','');
+        //     context.__proto__.__proto__.alert('位置：'+result);
+        //     if (result) {
+        //         let res = JSON.parse(result);
+        //         console.log('经纬度： ',res)
+        //         let params = {
+        //             token:cc.weijifen.authorization,
+        //             lng: res.lo,//j
+        //             lat: res.la,//w
+        //             alt:res.alt,
+        //             t:res.t,
+        //         } 
+        //         //  cc.sys.localStorage.setItem('tips','true');
+        //         cc.weijifen.http.httpPost('/userInfo/position/save',params,this.getPosition,this.getErr,this) ;            
+        //     }
+        // //  } 
         var param = {
             token:cc.weijifen.authorization,
             playway:cc.weijifen.playway,
